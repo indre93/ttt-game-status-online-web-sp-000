202dbe8b808c67884c8f42b20ae3c80244dffa2c
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-09 23:41:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,9 +16,6 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
- win_index_1 = win_combination[0]
- win_index_2 = win_combination[1]
- win_index_3 = win_combination[2]
   if board != WIN_COMBINATIONS || board == " "
    false 
   else 
