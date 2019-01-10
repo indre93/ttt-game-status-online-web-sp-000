49a36882cf35c365f582817e9166354f201bbb81
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-10 00:00:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,14 +17,19 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    if win_combination.all?{|index| index == "X" || index == "O"}
-      true
-    else 
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[3]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else
       false
     end
-  end 
+  end
 end
-
 
 
 
